d8bd92e5e1007c4725d73afabe6230736463d314
naveenpallagani777 <dev@codelabs.com>
2025-06-12 13:03:46 +0530

    changes for Vercel deployment setup

--- src/index.js
+++ src/index.js
@@ -1,30 +1,26 @@
+// src/app.js
 const express = require('express');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
-dotenv.config();
-
 const yakRouter = require('./routers/yak.router');
-const gobalErrorHandler = require('./controllers/error.controller');
+const globalErrorHandler = require('./controllers/error.controller');
+
+dotenv.config();
 
 const app = express();
 
 app.use(express.json());
 app.use('/api/yak-shop', yakRouter);
-app.use(gobalErrorHandler);
+app.use(globalErrorHandler);
 
 app.get('/', (req, res) => {
-    res.send('Welcome to the Yak API!');
+  res.send('Welcome to the Yak API!');
 });
 
-const port = process.env.PORT || 3000;
+// Connect to MongoDB
 const dbUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/mydatabase';
-// console.log(`Connecting to MongoDB at ${dbUri}`);
-mongoose.connect(dbUri
-).then(() => {
-    console.log('Connected to MongoDB');
-    app.listen(port, () => {
-        console.log(`Server is running on http://localhost:${port}`);
-    });
-}).catch(err => {
-    console.error('Error connecting to MongoDB:', err);
-});
+mongoose.connect(dbUri)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+module.exports = app;
