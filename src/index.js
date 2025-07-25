5dd6304e13eff21a847d391f6585ad0b5099e2a9
naveenpallagani777 <dev@codelabs.com>
2025-07-25 13:13:58 +0530

    deployment check 4

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ mongoose.connect(dbUri)
   .catch(err => console.error('MongoDB connection error:', err));
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log('CI/CD pipeline is set up successfully 2004!');
+  console.log('CI/CD pipeline is set up successfully 4!');
   console.log(`Server is running on port ${process.env.PORT || 3000}`);
 });
 module.exports = app;
